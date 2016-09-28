77830d0442d64e9db3a8271c627285f5460bce27
Shaylin Chetty <chtsha024@myuct.ac.za>
2016-09-28 22:51:46 +0200

    CPU benching enabled

--- numpy_correlator.py
+++ numpy_correlator.py
@@ -3,53 +3,51 @@ import matplotlib.pyplot as plt
 import time
 
 def main():
-	N_INPUTS = 2
-	N_OUTPUTS = int(N_INPUTS*(N_INPUTS+1)/2)
-	SIG_SIZE = 1000
+	benchsizes= [256,512,1024,2048,4096,
+						8192,16384,32768,65536,131072,
+						262144,524288,1048576,2097152,4194304]
+	resFile = open("cpures.csv","w")
 	
-	#Generating the input signals
-	inputArray = [None]*N_INPUTS
-	outputArray = [None]*N_OUTPUTS
-	for i in range(N_INPUTS):
-		inputArray[i] = sigGen(150.0, 8000, SIG_SIZE, True)
-
-	plt.figure(1)
-	for i in range(N_INPUTS):
-		plt.subplot(N_INPUTS,1,i+1)
-		plt.plot(inputArray[i])
-	plt.show()
-	
-	totalstart = time.clock()
-	fftstart = time.clock()
-	
-	RFFT(inputArray)
-	
-	fftend = time.clock()
-	multstart = time.clock()
-	
-	crossMult(inputArray, outputArray)
-	
-	multend = time.clock()
-	totalend = time.clock()
-	
-	print("FFT Time: "+str((fftend-fftstart)*1000)+" ms")	
-	print("Mult Time: "+str((multend-multstart)*1000)+" ms")
-	print("Total Time: "+str((totalend-totalstart)*1000)+" ms")
-	
-	#plt.figure(2)
-	#for i in range(N_INPUTS):
-		#plt.subplot(N_INPUTS,1,i+1)
-		#plt.plot(np.abs(inputArray[i]))
-	#plt.show()
-	
-	#plt.figure(3)
-	#for i in range(N_OUTPUTS):
-		#plt.subplot(N_OUTPUTS,1,i+1)
-		#plt.plot(np.abs(outputArray[i]))
-	#plt.show()
-	
+	for k in range(2,17):
+		N_INPUTS = k
+		N_OUTPUTS = int(N_INPUTS*(N_INPUTS+1)/2)
+		resFile.write(str(k)+"\n")
+		for j in range(15):
+			SIG_SIZE = benchsizes[j]
+			
+			#Generating the input signals
+			inputArray = [None]*N_INPUTS
+			outputArray = [None]*N_OUTPUTS
+			for i in range(N_INPUTS):
+				inputArray[i] = sigGen(150.0, 8000, SIG_SIZE, True)
+				
+			ffttime=0;
+			multtime=0;
+			
+			for i in range(11):
+				totalstart = time.clock()
+				fftstart = time.clock()
+				
+				RFFT(inputArray)
+				
+				fftend = time.clock()
+				multstart = time.clock()
+				
+				crossMult(inputArray, outputArray)
+				
+				multend = time.clock()
+				totalend = time.clock()
+				
+				if(i!=0):
+					ffttime=ffttime+(fftend-fftstart)
+					multtime=multtime+(multend-multstart)
+					
+			resFile.write(str(SIG_SIZE)+", "+str(ffttime*1000/10.0)+", "+str(multtime*1000/10.0)+"\n")
+			#print("FFT Time: "+str(ffttime*1000/10.0)+" ms")	
+			#print("Mult Time: "+str(multtime*1000/10.0)+" ms")
+			
+		resFile.write("\n")
 	return 0
-    
     
     
 def sigGen(f, fs, size, noise=False):
@@ -59,13 +57,7 @@ def sigGen(f, fs, size, noise=False):
 	else:
 		y = np.sin(2*np.pi*f*n/fs)
 		
-	padAmount = int(2**np.ceil(np.log2(2*size)))
-	yzer = np.zeros(padAmount)
-	
-	for i in range(size):
-		yzer[i] = yzer[i]+y[i]
-	
-	return yzer
+	return y
 
 def RFFT(inputArray):
 	for i in range(len(inputArray)):
